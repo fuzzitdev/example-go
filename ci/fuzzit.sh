006c14245afdb7d7367d0940e0ec937c333f930b
Yevgeny Pats <yp@fuzzit.dev>
2019-08-06 10:28:32 +0300

    Add local sanity test example

--- ci/fuzzit.sh
+++ ci/fuzzit.sh
@@ -3,13 +3,17 @@ set -xe
 ## Install go-fuzz
 go get -u github.com/dvyukov/go-fuzz/go-fuzz github.com/dvyukov/go-fuzz/go-fuzz-build
 
-## build and send to fuzzit
+## build fuzzer
 go build ./...
 go-fuzz-build -libfuzzer -o fuzzer.a ./...
 clang -fsanitize=fuzzer fuzzer.a -o fuzzer
 
-wget -q -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/download/v2.0.0/fuzzit_Linux_x86_64
+wget -q -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/download/v2.4.1/fuzzit_Linux_x86_64
 chmod a+x fuzzit
 ./fuzzit auth ${FUZZIT_API_KEY}
-export TARGET_ID=2n6hO2dQzylLxX5GGhRG
-./fuzzit create job --type $1 --branch $TRAVIS_BRANCH --revision $TRAVIS_COMMIT $TARGET_ID ./fuzzer
+
+if [ $1 == "fuzzing" ]; then
+    ./fuzzit create job --branch $TRAVIS_BRANCH --revision $TRAVIS_COMMIT parse-complex ./fuzzer
+else
+    ./fuzzit create job --local parse-complex ./fuzzer
+end
