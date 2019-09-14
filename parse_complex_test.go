6fc2fdc250eb2213b93a5506759f666ddcc31bda
Yevgeny Pats <yp@fuzzit.dev>
2019-09-14 16:23:15 +0300

    add generational fuzzing example

--- parse_complex_test.go
+++ parse_complex_test.go
@@ -1,10 +1,18 @@
 package parser
 
-import "testing"
+// This is just an example of a generational fuzzer which benchmarks the resutls against a coverage guided fuzzer
+// This won't find the results for never probabbly ...
+
+import (
+	"github.com/google/gofuzz"
+	"testing"
+)
 
 func TestParseComplex(t *testing.T) {
-	res := ParseComplex([]byte("Incorrect Data"))
-	if res {
-		t.Fail()
+	f := fuzz.New()
+	var inputString string
+	for true {
+		f.Fuzz(&inputString)
+		ParseComplex([]byte(inputString))
 	}
 }
