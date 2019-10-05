1a2499fd6afc412a654ff1d866abe6bfc4a653c3
Yevgeny Pats <yp@fuzzit.dev>
2019-10-05 18:19:37 +0300

    Update fuzzit.sh

--- fuzzit.sh
+++ fuzzit.sh
@@ -21,7 +21,7 @@ go-fuzz-build -libfuzzer -o parse-complex.a .
 clang -fsanitize=fuzzer parse-complex.a -o parse-complex
 
 ## Install fuzzit latest version:
-wget -q -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/latest/download/fuzzit_Linux_x86_64
+wget -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/latest/download/fuzzit_Linux_x86_64
 chmod a+x fuzzit
 
 ## upload fuzz target for long fuzz testing on fuzzit.dev server or run locally for regression
